2d9388a9f9afc272ab8573360ceef4901d80b344
kartiksinh <kartiksinh24@gmail.com>
2019-05-22 18:38:45 +0530

    added a functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Person from './Person/Person';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Kartik</h1>
+      <Person />
     </div>
   );
 }
